ae55702a926e164826c4d0d1640437a36fa0ecae
macle <ysys86a@gmail.com>
2019-11-11 07:40:43 +0900

    sync service update

--- src/main/java/com/seomse/sync/SyncService.java
+++ src/main/java/com/seomse/sync/SyncService.java
@@ -1,9 +1,11 @@
 package com.seomse.sync;
 
 import com.seomse.commons.config.Config;
+import com.seomse.commons.service.Service;
 import com.seomse.commons.utils.ExceptionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.security.util.PendingException;
 
 /**
  * <pre>
@@ -19,14 +21,12 @@ import org.slf4j.LoggerFactory;
  * </pre>
  * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
  */
-public class SyncService implements Runnable{
+public class SyncService extends Service {
 
 
     private static final Logger logger = LoggerFactory.getLogger(SyncService.class);
 
     private boolean isStop = false;
-
-    private long waitTime;
 
     //지연된시작
     private boolean isDelayedStart = false;
@@ -34,50 +34,25 @@ public class SyncService implements Runnable{
     /**
      * 생성자
      */
-    public SyncService(){
-        waitTime = Config.getLong("sync.service.wait.time", 3600000L);
-    }
-
-    /**
-     * 지연된 시작 설정
-     * @param delayedStart 지연된 시작 여부
-     */
-    public void setDelayedStart(boolean delayedStart) {
-        isDelayedStart = delayedStart;
+    public SyncService()
+    {
+        setSleepTime(Config.getLong("sync.service.sleep.time", 3600000L));
+        setState(State.START);
     }
 
     @Override
-    public void run() {
-
-        if(isStop){
-            return;
-        }
+    public void work() {
         try{
-
-            //처음에 시작되고 실행되므로 첫시작은 대기후 시작한다
-            if(isDelayedStart)
-                Thread.sleep(waitTime);
-
-            while (!isStop) {
-
-                SynchronizerManager synchronizerManager = SynchronizerManager.getInstance();
-                if (Config.getBoolean("sync.service.flag", true)
-                        && !synchronizerManager.isIng()) {
-                    synchronizerManager.sync();
-                }
-                Thread.sleep(waitTime);
+            SynchronizerManager synchronizerManager = SynchronizerManager.getInstance();
+            if (Config.getBoolean("sync.service.flag", true)
+                    && !synchronizerManager.isIng()) {
+                synchronizerManager.sync();
             }
-
-
         }catch(Exception e){
             logger.error(ExceptionUtil.getStackTrace(e));
         }
-
     }
 
 
-    public void stopService(){
-        isStop = true;
-    }
 
 }
